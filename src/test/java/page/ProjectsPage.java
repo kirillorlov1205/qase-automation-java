bcc653249bc3a6e5f0493a9191428e6e3fcccb26
Kiryl.Arlou <kirillorlov1205@gmail.com>
2023-10-08 20:50:06 +0300

    Projects page service

--- src/test/java/page/ProjectsPage.java
+++ src/test/java/page/ProjectsPage.java
@@ -1,6 +1,7 @@
 package page;
 
 import elementsWrappers.Button;
+import elementsWrappers.Input;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,7 +9,9 @@ import utils.Waiter;
 
 public class ProjectsPage extends BasePage {
 
-    private static final By CREATE_NEW_PROJECT_BUTTON = By.xpath("//button[@id='createButton']");
+    private static final By CREATE_NEW_PROJECT_BUTTON_LOCATOR = By.xpath("//button[@id='createButton']");
+    private static final By PROJECT_NAME_LOCATOR = By.xpath("//input[@id='project-name']");
+    private static final By PROJECT_CODE_LOCATOR = By.xpath("//input[@id='project-code']");
 
     @FindBy(xpath = "//table[contains(@class,'projects-table')]")
     private WebElement projectsTable;
@@ -18,7 +21,17 @@ public class ProjectsPage extends BasePage {
     }
 
     public ProjectsPage clickCreateNewProjectButton() {
-        new Button(CREATE_NEW_PROJECT_BUTTON);
+        new Button(CREATE_NEW_PROJECT_BUTTON_LOCATOR);
+        return this;
+    }
+
+    public ProjectsPage fillProjectName(String projectName){
+        new Input(PROJECT_NAME_LOCATOR).writeText(projectName);
+        return this;
+    }
+
+    public ProjectsPage fillProjectCode(String projectCode){
+        new Input(PROJECT_CODE_LOCATOR).writeText(projectCode);
         return this;
     }
 }
--- src/test/java/tests/ProjectsPageTest.java
+++ src/test/java/tests/ProjectsPageTest.java
@@ -1,4 +1,28 @@
 package tests;
 
-public class ProjectsPageTest {
+import io.qameta.allure.Description;
+import model.Constants;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import service.LoginPageService;
+
+public class ProjectsPageTest extends BaseTest {
+
+    private LoginPageService loginPageService;
+
+    @BeforeClass
+    public void setUp() {
+        loginPageService = new LoginPageService();
+    }
+
+//    @Test(description = "Verify successful project creation", priority = 1)
+//    @Description("Successful project creation")
+//    public void verifySuccessfulProjectCreation() {
+//        loginPageService.login(Constants.USER_WITH_VALID_CREDENTIALS)
+//
+//        String expectedValidationMessage = Constants.SUCCESSFUL_PASSWORD_RESET_VALIDATION_MESSAGE;
+//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+//                "match expected");
+//    }
 }
