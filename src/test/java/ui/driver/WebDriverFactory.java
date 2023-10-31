c337ed8f0c0c90c97bf55f9beb3120f27ca0f277
Kiryl.Arlou <kirillorlov1205@gmail.com>
2023-10-31 13:55:59 +0300

    jenkins 1

--- src/test/java/ui/driver/WebDriverFactory.java
+++ src/test/java/ui/driver/WebDriverFactory.java
@@ -34,7 +34,7 @@ public class WebDriverFactory {
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--incognito");
                     chromeOptions.addArguments("--remote-allow-origins=*");
-                    chromeOptions.addArguments("--window-size=1920,1080");
+                    chromeOptions.addArguments("--headless");
                     driver = new ChromeDriver(chromeOptions);
                 } catch (Exception e) {
                     log.fatal("Driver didn't start");
--- src/test/java/ui/tests/LoginPageTest.java
+++ src/test/java/ui/tests/LoginPageTest.java
@@ -6,7 +6,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ui.driver.UiDriverActions;
+import ui.model.User;
 import ui.service.LoginPageService;
+import ui.service.ProjectsListPageService;
 
 public class LoginPageTest extends BaseTest {
     private static final String INVALID_CREDENTIALS_VALIDATION_MESSAGE = "These credentials do not match our records.";
@@ -19,65 +21,65 @@ public class LoginPageTest extends BaseTest {
         loginPageService = new LoginPageService();
     }
 
-//    @Test(description = "Verify successful login with valid credentials", priority = 1)
-//    @Description("Login with valid credentials")
-//    public void verifySuccessfulLoginWithValidCredentials() {
-//        ProjectsListPageService projectsListPageService = loginPageService.login(new User());
-//        Assert.assertTrue(projectsListPageService.isProjectsPageDisplayed(), "Login failed");
-//    }
-//
-//    @Test(description = "Verify wrong email validation", priority = 2)
-//    @Description("Wrong email validation")
-//    public void verifyWrongEmailValidation() {
-//        User userWithWrongEmail = new User("wrongEmail@gmail.com", "TestingPass1!");
-//        loginPageService.login(userWithWrongEmail);
-//        String actualValidationMessage = loginPageService.getLoginValidationMessage();
-//        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
-//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-//                "match expected");
-//    }
-//
-//    @Test(description = "Verify wrong password validation", priority = 3, dataProvider = "Wrong passwords list")
-//    @Description("Wrong password validation")
-//    public void verifyWrongPasswordValidation(String wrongPassword) {
-//        User userWithWrongPassword = new User("test12051@mail.ru", wrongPassword);
-//        loginPageService.login(userWithWrongPassword);
-//        String actualValidationMessage = loginPageService.getLoginValidationMessage();
-//        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
-//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-//                "match expected");
-//    }
-//
-//    @Test(description = "Verify empty email validation", priority = 4)
-//    @Description("Empty email validation")
-//    public void verifyEmptyEmailValidation() {
-//        User userWithEmptyEmail = new User("", "TestingPass1!");
-//        loginPageService.login(userWithEmptyEmail);
-//        Assert.assertTrue(loginPageService.isEmptyEmailValidationMessageDisplayed(), "Empty email " +
-//                "validation message hasn't been shown");
-//    }
-//
-//    @Test(description = "Verify empty password validation", priority = 5)
-//    @Description("Empty password validation")
-//    public void verifyEmptyPasswordValidation() {
-//        User userWithEmptyPassword = new User("test12051@mail.ru", "");
-//        loginPageService.login(userWithEmptyPassword);
-//        Assert.assertTrue(loginPageService.isEmptyPasswordValidationMessageDisplayed(), "Empty password " +
-//                "validation message hasn't been shown");
-//    }
-//
-//    @Test(description = "Verify wrong email format validation", priority = 6, dataProvider = "Wrong format emails list",
-//            dataProviderClass = DataProviders.class)
-//    @Description("Wrong email format validation")
-//    public void verifyWrongEmailFormatValidation(String email) {
-//        User userWithWrongFormatEmail = new User(email);
-//        loginPageService.login(userWithWrongFormatEmail);
-//        String actualValidationMessage = loginPageService.getWrongEmailFormatValidationMessage();
-//        String expectedValidationMessage = String.format(INVALID_EMAIL_FORMAT_VALIDATION_MESSAGE,
-//                userWithWrongFormatEmail.getEmail());
-//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-//                "match expected");
-//    }
+    @Test(description = "Verify successful login with valid credentials", priority = 1)
+    @Description("Login with valid credentials")
+    public void verifySuccessfulLoginWithValidCredentials() {
+        ProjectsListPageService projectsListPageService = loginPageService.login(new User());
+        Assert.assertTrue(projectsListPageService.isProjectsPageDisplayed(), "Login failed");
+    }
+
+    @Test(description = "Verify wrong email validation", priority = 2)
+    @Description("Wrong email validation")
+    public void verifyWrongEmailValidation() {
+        User userWithWrongEmail = new User("wrongEmail@gmail.com", "TestingPass1!");
+        loginPageService.login(userWithWrongEmail);
+        String actualValidationMessage = loginPageService.getLoginValidationMessage();
+        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
+        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+                "match expected");
+    }
+
+    @Test(description = "Verify wrong password validation", priority = 3, dataProvider = "Wrong passwords list")
+    @Description("Wrong password validation")
+    public void verifyWrongPasswordValidation(String wrongPassword) {
+        User userWithWrongPassword = new User("test12051@mail.ru", wrongPassword);
+        loginPageService.login(userWithWrongPassword);
+        String actualValidationMessage = loginPageService.getLoginValidationMessage();
+        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
+        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+                "match expected");
+    }
+
+    @Test(description = "Verify empty email validation", priority = 4)
+    @Description("Empty email validation")
+    public void verifyEmptyEmailValidation() {
+        User userWithEmptyEmail = new User("", "TestingPass1!");
+        loginPageService.login(userWithEmptyEmail);
+        Assert.assertTrue(loginPageService.isEmptyEmailValidationMessageDisplayed(), "Empty email " +
+                "validation message hasn't been shown");
+    }
+
+    @Test(description = "Verify empty password validation", priority = 5)
+    @Description("Empty password validation")
+    public void verifyEmptyPasswordValidation() {
+        User userWithEmptyPassword = new User("test12051@mail.ru", "");
+        loginPageService.login(userWithEmptyPassword);
+        Assert.assertTrue(loginPageService.isEmptyPasswordValidationMessageDisplayed(), "Empty password " +
+                "validation message hasn't been shown");
+    }
+
+    @Test(description = "Verify wrong email format validation", priority = 6, dataProvider = "Wrong format emails list",
+            dataProviderClass = DataProviders.class)
+    @Description("Wrong email format validation")
+    public void verifyWrongEmailFormatValidation(String email) {
+        User userWithWrongFormatEmail = new User(email);
+        loginPageService.login(userWithWrongFormatEmail);
+        String actualValidationMessage = loginPageService.getWrongEmailFormatValidationMessage();
+        String expectedValidationMessage = String.format(INVALID_EMAIL_FORMAT_VALIDATION_MESSAGE,
+                userWithWrongFormatEmail.getEmail());
+        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+                "match expected");
+    }
 
     @Test(description = "Verify additional link transferring", priority = 7, dataProvider = "Additional links list")
     @Description("Additional link transferring")
