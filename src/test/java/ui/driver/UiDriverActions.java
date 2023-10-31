00296ab043259cab8492a054675e9bf270e90d84
Kiryl.Arlou <kirillorlov1205@gmail.com>
2023-10-31 11:57:45 +0300

    jenkins 1

--- src/test/java/ui/driver/UiDriverActions.java
+++ src/test/java/ui/driver/UiDriverActions.java
@@ -1,6 +1,7 @@
 package ui.driver;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 public class UiDriverActions {
     private UiDriverActions() {
@@ -14,13 +15,31 @@ public class UiDriverActions {
         DriverSingleton.getInstance().getDriver().get(url);
     }
 
-    public static String getSecondOpenedTabUrl() {
-        ArrayList<String> tabs = new ArrayList(DriverSingleton.getInstance().getDriver().getWindowHandles());
-        DriverSingleton.getInstance().getDriver().switchTo().window(tabs.get(1));
-        String pageUrl = UiDriverActions.getCurrentUrl();
-        DriverSingleton.getInstance().getDriver().close();
-        DriverSingleton.getInstance().getDriver().switchTo().window(tabs.get(0));
-        return pageUrl;
+    public static boolean isAdditionalPageOpened(String pageUrl) {
+        boolean isPageOpened = false;
+        String MainWindow = DriverSingleton.getInstance().getDriver().getWindowHandle();
+        Set<String> s1 = DriverSingleton.getInstance().getDriver().getWindowHandles();
+        Iterator<String> i1 = s1.iterator();
+
+        while (i1.hasNext()) {
+            String ChildWindow = i1.next();
+            if (!MainWindow.equalsIgnoreCase(ChildWindow)) {
+                String currentUrl = DriverSingleton.getInstance().getDriver().switchTo().window(ChildWindow).getCurrentUrl();
+                if (currentUrl.contains(pageUrl)) {
+                    isPageOpened = true;
+                }
+                DriverSingleton.getInstance().getDriver().switchTo().window(MainWindow);
+//                DriverSingleton.getInstance().getDriver().close();
+            }
+        }
+        return isPageOpened;
+
+//        ArrayList<String> tabs = new ArrayList(DriverSingleton.getInstance().getDriver().getWindowHandles());
+//        DriverSingleton.getInstance().getDriver().switchTo().window(tabs.get(1));
+//        String pageUrl = UiDriverActions.getCurrentUrl();
+//        DriverSingleton.getInstance().getDriver().close();
+//        DriverSingleton.getInstance().getDriver().switchTo().window(tabs.get(0));
+//        return pageUrl;
     }
 
     public static void acceptAlert() {
--- src/test/java/ui/tests/LoginPageTest.java
+++ src/test/java/ui/tests/LoginPageTest.java
@@ -22,73 +22,73 @@ public class LoginPageTest extends BaseTest {
         loginPageService = new LoginPageService();
     }
 
-    @Test(description = "Verify successful login with valid credentials", priority = 1)
-    @Description("Login with valid credentials")
-    public void verifySuccessfulLoginWithValidCredentials() {
-        ProjectsListPageService projectsListPageService = loginPageService.login(new User());
-        Assert.assertTrue(projectsListPageService.isProjectsPageDisplayed(), "Login failed");
-    }
-
-    @Test(description = "Verify wrong email validation", priority = 2)
-    @Description("Wrong email validation")
-    public void verifyWrongEmailValidation() {
-        User userWithWrongEmail = new User("wrongEmail@gmail.com", "TestingPass1!");
-        loginPageService.login(userWithWrongEmail);
-        String actualValidationMessage = loginPageService.getLoginValidationMessage();
-        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
-        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-                "match expected");
-    }
-
-    @Test(description = "Verify wrong password validation", priority = 3, dataProvider = "Wrong passwords list")
-    @Description("Wrong password validation")
-    public void verifyWrongPasswordValidation(String wrongPassword) {
-        User userWithWrongPassword = new User("test12051@mail.ru", wrongPassword);
-        loginPageService.login(userWithWrongPassword);
-        String actualValidationMessage = loginPageService.getLoginValidationMessage();
-        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
-        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-                "match expected");
-    }
-
-    @Test(description = "Verify empty email validation", priority = 4)
-    @Description("Empty email validation")
-    public void verifyEmptyEmailValidation() {
-        User userWithEmptyEmail = new User("", "TestingPass1!");
-        loginPageService.login(userWithEmptyEmail);
-        Assert.assertTrue(loginPageService.isEmptyEmailValidationMessageDisplayed(), "Empty email " +
-                "validation message hasn't been shown");
-    }
-
-    @Test(description = "Verify empty password validation", priority = 5)
-    @Description("Empty password validation")
-    public void verifyEmptyPasswordValidation() {
-        User userWithEmptyPassword = new User("test12051@mail.ru", "");
-        loginPageService.login(userWithEmptyPassword);
-        Assert.assertTrue(loginPageService.isEmptyPasswordValidationMessageDisplayed(), "Empty password " +
-                "validation message hasn't been shown");
-    }
-
-    @Test(description = "Verify wrong email format validation", priority = 6, dataProvider = "Wrong format emails list",
-            dataProviderClass = DataProviders.class)
-    @Description("Wrong email format validation")
-    public void verifyWrongEmailFormatValidation(String email) {
-        User userWithWrongFormatEmail = new User(email);
-        loginPageService.login(userWithWrongFormatEmail);
-        String actualValidationMessage = loginPageService.getWrongEmailFormatValidationMessage();
-        String expectedValidationMessage = String.format(INVALID_EMAIL_FORMAT_VALIDATION_MESSAGE,
-                userWithWrongFormatEmail.getEmail());
-        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
-                "match expected");
-    }
+//    @Test(description = "Verify successful login with valid credentials", priority = 1)
+//    @Description("Login with valid credentials")
+//    public void verifySuccessfulLoginWithValidCredentials() {
+//        ProjectsListPageService projectsListPageService = loginPageService.login(new User());
+//        Assert.assertTrue(projectsListPageService.isProjectsPageDisplayed(), "Login failed");
+//    }
+//
+//    @Test(description = "Verify wrong email validation", priority = 2)
+//    @Description("Wrong email validation")
+//    public void verifyWrongEmailValidation() {
+//        User userWithWrongEmail = new User("wrongEmail@gmail.com", "TestingPass1!");
+//        loginPageService.login(userWithWrongEmail);
+//        String actualValidationMessage = loginPageService.getLoginValidationMessage();
+//        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
+//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+//                "match expected");
+//    }
+//
+//    @Test(description = "Verify wrong password validation", priority = 3, dataProvider = "Wrong passwords list")
+//    @Description("Wrong password validation")
+//    public void verifyWrongPasswordValidation(String wrongPassword) {
+//        User userWithWrongPassword = new User("test12051@mail.ru", wrongPassword);
+//        loginPageService.login(userWithWrongPassword);
+//        String actualValidationMessage = loginPageService.getLoginValidationMessage();
+//        String expectedValidationMessage = INVALID_CREDENTIALS_VALIDATION_MESSAGE;
+//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+//                "match expected");
+//    }
+//
+//    @Test(description = "Verify empty email validation", priority = 4)
+//    @Description("Empty email validation")
+//    public void verifyEmptyEmailValidation() {
+//        User userWithEmptyEmail = new User("", "TestingPass1!");
+//        loginPageService.login(userWithEmptyEmail);
+//        Assert.assertTrue(loginPageService.isEmptyEmailValidationMessageDisplayed(), "Empty email " +
+//                "validation message hasn't been shown");
+//    }
+//
+//    @Test(description = "Verify empty password validation", priority = 5)
+//    @Description("Empty password validation")
+//    public void verifyEmptyPasswordValidation() {
+//        User userWithEmptyPassword = new User("test12051@mail.ru", "");
+//        loginPageService.login(userWithEmptyPassword);
+//        Assert.assertTrue(loginPageService.isEmptyPasswordValidationMessageDisplayed(), "Empty password " +
+//                "validation message hasn't been shown");
+//    }
+//
+//    @Test(description = "Verify wrong email format validation", priority = 6, dataProvider = "Wrong format emails list",
+//            dataProviderClass = DataProviders.class)
+//    @Description("Wrong email format validation")
+//    public void verifyWrongEmailFormatValidation(String email) {
+//        User userWithWrongFormatEmail = new User(email);
+//        loginPageService.login(userWithWrongFormatEmail);
+//        String actualValidationMessage = loginPageService.getWrongEmailFormatValidationMessage();
+//        String expectedValidationMessage = String.format(INVALID_EMAIL_FORMAT_VALIDATION_MESSAGE,
+//                userWithWrongFormatEmail.getEmail());
+//        Assert.assertEquals(actualValidationMessage, expectedValidationMessage, "Validation message doesn't " +
+//                "match expected");
+//    }
 
     @Test(description = "Verify additional link transferring", priority = 7, dataProvider = "Additional links list")
     @Description("Additional link transferring")
     public void verifyAdditionalLinkTransferring(String linkName, String expectedPageUrl) {
         loginPageService.openLoginPage()
                 .clickOnAdditionalLinkByName(linkName);
-        String actualPageUrl = UiDriverActions.getSecondOpenedTabUrl();
-        Assert.assertTrue(actualPageUrl.contains(expectedPageUrl), "Page url doesn't match expected");
+//        String actualPageUrl = UiDriverActions.isAdditionalPageOpened();
+        Assert.assertTrue(UiDriverActions.isAdditionalPageOpened(expectedPageUrl), "Page url doesn't match expected");
     }
 
     @Test(description = "Verify live chat opening", priority = 8)
